bd69518727f2b5b54683db7b311fced6a8a8e066
yashaswipratick <yashaswi.pratick@gmail.com>
2026-09-04 19:32:24 +0530

    Raw footage analysis

--- youtube-analytics/src/main/java/com/youtube/analytics/YoutubeAnalyticsApplication.java
+++ youtube-analytics/src/main/java/com/youtube/analytics/YoutubeAnalyticsApplication.java
@@ -2,8 +2,11 @@ package com.youtube.analytics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import com.youtube.analytics.videoanalysis.ingestion.LocalMediaInputProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(LocalMediaInputProperties.class)
 public class YoutubeAnalyticsApplication {
 
     public static void main(String[] args) {
